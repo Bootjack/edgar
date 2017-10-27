768e5455c0be89933c51e921c0e9d2205ecce2a3
Jason Hinebaugh <jason.hinebaugh@customink.com>
2017-10-26 23:32:32 -0400

    Export for umd; bump version

--- dist/edgar.js
+++ dist/edgar.js
@@ -1,4 +1,14 @@
-/******/ (function(modules) { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["edgar"] = factory();
+	else
+		root["edgar"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -164,3 +174,4 @@ function testEdges(src, options) {
 
 /***/ })
 /******/ ]);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'edgar.js',
-    library: 'umd'
+    library: 'edgar',
+    libraryTarget: 'umd'
   },
   module: {
     rules: [{
